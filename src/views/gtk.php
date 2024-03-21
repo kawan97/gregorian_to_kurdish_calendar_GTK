fb13623507526808c4a5eb5da82c178084965147
kawan97 <pshtiwankawan@gmail.com>
2024-03-21 07:01:15 -0700

    change local decomentaion

--- src/views/gtk.php
+++ src/views/gtk.php
@@ -4,12 +4,102 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>gregorian_to_kurdish_calendar</title>
+    <title>Gregorian To Kurdish Calendar Laravel Package</title>
+    <style>
+        body {
+            margin: 20px;
+            background-color: #212124;
+            color: white;
+        }
+        a {
+            color: white;
+            font-weight: bold;
+        }
+        pre {
+            background-color: black;
+            color: white;
+            padding: 5px 10px;
+            border-radius: 15px;
+        }  
+    </style>
 </head>
-<body style="">
-    <center style="">
-        <h1>gregorian_to_kurdish_calendar</h1>
-    </center>
-    
+<body >
+
+<h1>Gregorian To Kurdish Calendar Laravel Package</h1>
+<p>
+    <a href="https://packagist.org/packages/kawan/gregorian_to_kurdish_calendar">
+        <img src="https://img.shields.io/packagist/dt/kawan/gregorian_to_kurdish_calendar" alt="Total Downloads">
+    </a>
+    <a href="https://packagist.org/packages/kawan/gregorian_to_kurdish_calendar">
+        <img src="https://img.shields.io/packagist/v/kawan/gregorian_to_kurdish_calendar" alt="Latest Version">
+    </a>
+</p>
+
+<p>This package allows you to convert dates from the Gregorian calendar to the Kurdish calendar.</p>
+
+<h2>Installation</h2>
+
+<p>You can install the "kawan/gregorian_to_kurdish_calendar" package via Composer by running the following command:</p>
+
+<pre><code>composer require kawan/gregorian_to_kurdish_calendar
+</code></pre>
+
+<h2>Usage</h2>
+
+<p>After installation, you can use the <code>gregorian_to_kurdish($date,$format)</code> function to convert dates. The function accepts two parameters: the date to convert and the desired output format.</p>
+
+<ul>
+    <li><code>$date</code>: The date you want to convert.</li>
+    <li><code>$format</code>: The desired output format. You can use standard PHP date format letters (Y, y, m, d, l, F, a, G, g, H, h, i, s). Any other letter will be returned as is.</li>
+</ul>
+
+<pre><code>$date = "2024-03-21";
+$format = "Y-d-m";
+echo gregorian_to_kurdish($date, $format);
+</code></pre>
+
+<h2>Local Documentation</h2>
+
+<p>Upon installation, this package creates a local documentation route. You can access it at <code>your_domain/gtk-test</code>.</p>
+
+<h2>Examples</h2>
+
+<p>Here are some examples of using the package:</p>
+
+<pre><code>// Example 1
+$date = "2024-02-25";
+$format = "d-m-Y | F";
+echo gregorian_to_kurdish($date, $format);
+//output:  "٥-١٢-٢٧٢٣ | ڕەشەمێ" 
+
+// Example 2
+$date = "2024-03-21 13:34:15";
+$format = "Y - m - d | l | F | a |g:i:s";
+echo gregorian_to_kurdish($date, $format);
+//output: "٢٧٢٤ - ١ - ١ | ٥ شەمە | نەورۆز | د.ن |١:٣٤:١٥"
+</code></pre>
+
+<h2>License</h2>
+
+<p>This package is open-source software licensed under the <a href="https://github.com/kawan97/gregorian_to_kurdish_calendar_GTK/blob/main/LICENSE">MIT License</a>.</p>
+
+<h2>Support</h2>
+
+<p>If you encounter any issues or have questions, feel free to open an issue on the <a href="https://github.com/kawan97/gregorian_to_kurdish_calendar_GTK/issues">GitHub repository</a>.</p>
+
+<h2>Credits</h2>
+
+<p>This package is developed and maintained by:</p>
+
+<ul>
+    <li><a href="https://github.com/kawan97">Kawan Pshtiwan</a></li>
+</ul>
+
+<p>And with contributions from our amazing community of open-source developers.</p>
+
+<h2>Changelog</h2>
+
+<p>See the <a href="https://github.com/kawan97/gregorian_to_kurdish_calendar_GTK/blob/main/CHANGELOG.md">CHANGELOG</a> file for information about the latest updates and changes.</p>
+
 </body>
 </html>
